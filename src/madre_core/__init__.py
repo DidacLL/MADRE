e3254ade0f093c2b7ca6ffa0d8bfc10161b56840
Didac Ll. <107514286+DidacLL@users.noreply.github.com>
2026-09-07 03:26:28 +0200

    Export concrete CORE planning behavior

--- src/madre_core/__init__.py
+++ src/madre_core/__init__.py
@@ -2,12 +2,28 @@
 
 from madre_core.client import CORE_APPLICATION_ID, CoreClient, CoreRuntimeError
 from madre_core.interaction import CoreConversation, CoreTurn, ReasoningRecommendation
+from madre_core.planning import (
+    CorePlanningAgent,
+    CorePlanningWorkflow,
+    CoreWorkPlan,
+    CoreWorkPlanError,
+    WorkPlanStep,
+    create_reviewed_planning_work_plan,
+    execute_reviewed_planning_work_plan,
+)
 
 __all__ = [
     "CORE_APPLICATION_ID",
     "CoreClient",
     "CoreConversation",
+    "CorePlanningAgent",
+    "CorePlanningWorkflow",
     "CoreRuntimeError",
     "CoreTurn",
+    "CoreWorkPlan",
+    "CoreWorkPlanError",
     "ReasoningRecommendation",
+    "WorkPlanStep",
+    "create_reviewed_planning_work_plan",
+    "execute_reviewed_planning_work_plan",
 ]
